d4ce1ae2b07ea568db0bc20ab8581a99393807fc
VladislavZhyliakov <80688590+VladislavZhyliakov@users.noreply.github.com>
2024-04-22 20:16:21 +0300

    Made StyledText more reusable.

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.outputText, {super.key});
+
+  final String outputText;
 
   @override
   Widget build(context) {
-    return const Text(
-      'This is text.',
-      style: TextStyle(
+    return Text(
+      outputText,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
